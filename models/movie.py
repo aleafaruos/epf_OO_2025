234e7883a227c4d90e1b8fba519f6d26682b1da9
Rafaela <rafaela0118@gmail.com>
2025-06-24 19:19:58 -0300

    mudança de nome da classe filme para movie.py

---
+++ models/movie.py
@@ -0,0 +1,29 @@
+class MovieModel:
+    def __init__(self, id, title, director, year, genre, synopsis):
+        self.id = id
+        self.title = title
+        self.director = director
+        self.year = year
+        self.genre = genre
+        self.synopsis = synopsis
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'director': self.director,
+            'year': self.year,
+            'genre': self.genre,
+            'synopsis': self.synopsis
+        }
+
+    @classmethod
+    def from_dict(cls, data):    
+        return cls(
+            id=data.get('id'),
+            title=data.get('title'),
+            director=data.get('director'),
+            year=data.get('year'),
+            genre=data.get('genre'),
+            synopsis=data.get('synopsis')
+        )
