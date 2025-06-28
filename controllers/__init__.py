48df226f43067feca25c5a81c0c71b4e930da321
Rafaela <rafaela0118@gmail.com>
2025-06-27 21:10:39 -0300

    adição da rota de movie controler

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,10 @@
 from bottle import Bottle
 from controllers.user_controller import user_routes
-from controllers.base_controller import BaseController 
+from controllers.movie_controller import movie_routes
 
 def init_controllers(app: Bottle):
     app.merge(user_routes)
+    app.merge(movie_routes)
    
 
 
